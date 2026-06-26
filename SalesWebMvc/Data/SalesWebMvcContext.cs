d5cb605aced81fd19b84d273a6fbf6aead0982e3
Emerson Santana <emersonsouzasantana@outlook.com>
2026-06-26 13:55:02 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,7 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+
 namespace SalesWebMvc.Data;
 
 public class SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options) : DbContext(options)
 {
-    public DbSet<SalesWebMvc.Models.Department> Department { get; set; } = default!;
+    public DbSet<Department> Department { get; set; } = default!;
+    public DbSet<Seller> Seller { get; set; } = default!;
+    public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
 }
+ 
