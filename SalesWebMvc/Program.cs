eec15db0079dcad371afb5f6cbbc8468a7f227a6
Emerson Santana <emersonsouzasantana@outlook.com>
2026-06-27 12:49:32 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -24,6 +24,7 @@ namespace SalesWebMvc
             builder.Services.AddControllersWithViews();
             builder.Services.AddScoped<SeedingService>();
             builder.Services.AddScoped<SellerService>();
+            builder.Services.AddScoped<DepartmentService>();
 
             var app = builder.Build();
 
