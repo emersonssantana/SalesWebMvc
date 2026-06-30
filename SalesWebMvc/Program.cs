e5043195c54ee4f8b538584c8537cbc4a10b2a6a
Emerson Santana <emersonsouzasantana@outlook.com>
2026-06-30 11:37:11 -0300

    App locale, number and date formatting

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,4 +1,7 @@
+using System.Globalization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
 using Pomelo.EntityFrameworkCore.MySql;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
@@ -12,6 +15,7 @@ namespace SalesWebMvc
             var builder = WebApplication.CreateBuilder(args);
             var connectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.");
 
+            // --- SERVICES CONFIGURATION (Dependency Injection Container) ---
             builder.Services.AddDbContext<SalesWebMvcContext>(options =>
             {
                 options.UseMySql(
@@ -28,13 +32,26 @@ namespace SalesWebMvc
 
             var app = builder.Build();
 
+            // --- MIDDLEWARES CONFIGURATION (HTTP Request Pipeline) ---
+
+            // Configure Request Localization (en-US)
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+            app.UseRequestLocalization(localizationOptions);
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                // Seed database on application startup
                 using (var scope = app.Services.CreateScope())
                 {
                     var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
-                    seedingService.Seed(); 
+                    seedingService.Seed();
                 }
 
                 app.UseDeveloperExceptionPage();
